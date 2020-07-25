d29e0279ed3d09f946dbca0fe7e0df1501ed30d5
Aleem Isiaka <aleemisiaka@gmail.com>
2020-07-25 10:25:33 +0100

    Source plugin installation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,41 @@
 module.exports = {
   siteMetadata: {},
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "assets",
+        path: `${__dirname}/static/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 690,
+            },
+          },
+          {
+            resolve: "gatsby-remark-responsive-iframe",
+          },
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-autolink-headers",
+          "gatsby-remark-prismjs",
+        ],
+      },
+    },
+  ],
 };
