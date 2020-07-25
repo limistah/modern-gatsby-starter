0ba950a7a6c24f1db30ab0920a36b20b20e2e91b
Aleem Isiaka <aleemisiaka@gmail.com>
2020-07-25 10:45:28 +0100

    Fetches all markdown using a query

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,4 +2,35 @@
 
 exports.onCreateNode = ({ node, actions, getNode }) => {};
 
-exports.createPages = async ({ graphql, actions }) => {};
+exports.createPages = async ({ graphql, actions }) => {
+  // Pulls the createPage action from the Actions API
+  const { createPage } = actions;
+
+  // Get all the markdown parsed through the help of gatsby-source-filesystem and gatsby-transformer-remark
+  const allMarkdownResult = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              tags
+              category
+              date
+              author
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  // Throws if any error occur while fetching the markdown files
+  if (allMarkdownResult.errors) {
+    console.error(allMarkdownResult.errors);
+    throw allMarkdownResult.errors;
+  }
+};
